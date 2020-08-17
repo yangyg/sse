48ad24e26e5d4a13e3ef343e87b7d848611ae80c
yyg <yonggan.yang@email.com>
2020-08-17 16:06:01 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,28 +9,31 @@ import (
 )
 
 func main() {
-	r := gin.Default()
+	r := gin.Default() // gin 初始化路由
+	// 返回index.html
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", "")
 	})
 
+	// sse路由 返回事件流
 	r.GET("/sse", sse)
 
 	r.LoadHTMLGlob("index.html")
 
-	r.Run(":6788") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":6788") // listen and serve on 0.0.0.0:6788 (for windows "localhost:6788")
 }
 
 func sse(c *gin.Context) {
 	c.Header("Content-Type", "text/event-stream") // 规定把报头设置为 text/event-stream
 	c.Header("Cache-Control", "no-cache")         // 设置不对页面进行缓存
 
-	timer := time.NewTimer(2 * time.Second)
+	timer := time.NewTimer(2 * time.Second) // 设置定时器
+	// 返回流
 	c.Stream(func(w io.Writer) bool {
 		for {
 			select {
 			case <-timer.C:
-				msg := "msg"
+				msg := "测试msg"
 				c.SSEvent("message", msg)
 				timer.Reset(2 * time.Second) // 上一个执行完毕重新设置
 				return true
